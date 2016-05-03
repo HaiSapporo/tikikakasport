ea4e17874c02838e8b2c54201bd303eaaaefe0bc
HaiSapporo <HaiPS1@fsoft.com.vn>
2016-05-03 17:08:39 +0700

    update source code
    update soucre code full

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/tikikakasport/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/tikikakasport/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/tikikakasport/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/tikikakasport/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/tikikakasport/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/tikikakasport/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/tikikakasport/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/tikikakasport/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/tikikakasport/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/tikikakasport/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/tikikakasport/system/upload/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/tikikakasport/system/modification/');
+define('DIR_LOGS', 'C:/xampp/htdocs/tikikakasport/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tikikakasport');
+define('DB_PREFIX', 'oc_');
